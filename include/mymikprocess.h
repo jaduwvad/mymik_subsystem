eb8854e9ed136e1a9341219b357d9852912393b1
jaduwvad2 <jadwvad2@gmail.com>
2018-11-30 17:10:58 +0100

    add description in header files

--- include/mymikprocess.h
+++ include/mymikprocess.h
@@ -5,17 +5,25 @@
 
 class MymikProcess {
 public:
+    //Constructor : set data file name, result file's directory, config index name
     MymikProcess();
     ~MymikProcess();
 
-    void getSrcData(Json::Array::const_iterator ci, std::string filename, std::vector<Json::Object>& srcData);
+    //get shop data in json format
     void getShopData(Json::Array& shopData);
+    //get article's data by tag and get specific field in Object vector
     void getESData(std::vector<Json::Object>& esData, std::string tag);
+    //get article's specific column data in src file from affilinet
+    void getSrcData(Json::Array::const_iterator ci, std::string filename, std::vector<Json::Object>& srcData);
 
+    //match two article list. One from affilinet, one from elasticsearch. Check whether price updated
     void matchingList(std::vector<Json::Object>& srcData, std::vector<Json::Object>& esData, std::vector<Json::Object>& result);
+    //Write matched articles in result file. Write article's price data if price's updated
     void setPriceInven(std::vector<Json::Object>& matchedData, std::string filename);
-    void setUpdatedData(Json::Object& updatedData, Json::Object& srcData, std::string price, bool priceUpdate);
 
+    //make Json Object by matched article data.
+    void setUpdatedData(Json::Object& updatedData, Json::Object& srcData, std::string price, bool priceUpdate);
+    //parse supplier number : delete 'P' in VTS's ArtNumber and delete '0' in front of ArtNumber
     std::string formSupplierNumber(std::string spn);
     void showTime();
 
--- include/srcdatafile.h
+++ include/srcdatafile.h
@@ -14,21 +14,28 @@ namespace ThreatShopData {
         SrcDataFile(std::string url, std::string filename, const char delimiter, std::string userId, std::string userPw);
         //Add unzip code in the end of constructor
         SrcDataFile(std::string url, std::string filename, const char delimiter, bool zipped);
-        //close filestream, delete file
+
+        //Destructor : close filestream, delete downloaded file
         ~SrcDataFile();
 
-        //read spacific column`s value in csv file
+        //read specific column`s value in csv file
         void readColumn(std::vector<std::string> columnHeaders, std::vector<Json::Object>& result);
         
-        
     private:
- 
+        //open filestream in ifstream _srcFile, and get header data in vector _header
         void setFileStream();
+
+        //Default file download function by url
         void setCurl(const std::string url);
+        //file download by url with login - uncompleted
         void setCurl(const std::string url, std::string userId, std::string userPw);
+
+        //get encoded string by base64. used in login
         std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len);
+        //separate data by _delimiter. delete '"'
         void myExplode(std::string s, std::vector<std::string>& result);
 
+        //function for download file
         static size_t writeData(void *ptr, size_t size, size_t nmemb, FILE *stream){
             return fwrite(ptr, size, nmemb, stream);
         }
